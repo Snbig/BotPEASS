64d3fd96120edd267b1fd3f19172d3bf5af06801
Alireza Mokhtari <55655007+MX-Solo@users.noreply.github.com>
2024-03-09 16:13:26 +0330

    keyword

--- botpeas.py
+++ botpeas.py
@@ -200,8 +200,9 @@ def generate_new_cve_message(cve_data: dict) -> str:
     ''' Generate new CVE message for sending to slack '''
 
     message = f"🚨  *{cve_data['id']}*  🚨\n"
+    keyword = cve_data['keyword'].replace(" ","_")
 
-    message += f"#️⃣ *keyword* : #{cve_data['keyword'].replace(" ","_")}  \n"
+    message += f"#️⃣ *keyword* : #{cve_data['keyword']}  \n"
     message += f"🔮  *CVSS*: {cve_data['cvss']}\n"
     message += f"📅  *Published*: {cve_data['Published']}\n"
     message += "📓  *Summary*: " 
